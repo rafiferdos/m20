fc90df27e81f48d9db4fa60f423be53162ae1671
Rafi Ferdos <rafiferdos@gmail.com>
2026-06-27 21:15:56 +0600

    feat: set up CORS and middleware for JSON parsing and cookie handling

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,19 @@
 import type { Application, Request, Response } from 'express'
 import express from 'express'
+import cors from 'cors'
+import config from './config/index.js'
+import cookieParser from 'cookie-parser'
 
 const app: Application = express()
+
+app.use(cors({
+  origin: config.app_url,
+  credentials: true,
+}))
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser())
 
 app.get('/', (_req: Request, res: Response) => {
 	res.send('Hello World!')
