2b30280187cfa5a8642aee601a66b1c311bab961
Rafi Ferdos <rafiferdos@gmail.com>
2026-06-29 22:48:33 +0600

    feat: create auth route for user login

---
+++ src/modules/auth/auth.route.ts
@@ -0,0 +1,8 @@
+import { Router } from "express";
+import { AuthControllers } from "./auth.controller.js";
+
+const router = Router()
+
+router.post('/login', AuthControllers.login)
+
+export const AuthRoutes : Router= router
