a58a4f06e44c957dff495399e14c444d4e996037
Rafi Ferdos <rafiferdos@gmail.com>
2026-06-29 22:51:56 +0600

    feat: implement loginUser controller to handle user authentication

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,7 +1,19 @@
 import catchAsync from '@/utils/catchAsync.js'
+import sendResponse from '@/utils/sendResponse.js'
 import type { Request, Response } from 'express'
+import status from 'http-status'
+import { AuthServices } from './auth.service.js'
 
-const loginUser = catchAsync(async (req: Request, res: Response) => {})
+const loginUser = catchAsync(async (req: Request, res: Response) => {
+	const payload = req.body
+	const result = await AuthServices.login(payload)
+
+	sendResponse(res, {
+		statusCode: status.OK,
+		message: 'User logged in successfully',
+		data: result
+	})
+})
 
 export const AuthControllers = {
 	login: loginUser
