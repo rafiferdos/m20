8fdd3258a53264e8d9ab4f0bc47d4d543df153e5
Rafi Ferdos <rafiferdos@gmail.com>
2026-06-30 17:45:50 +0600

    feat: refactor loginUserIntoDB to be asynchronous for improved user authentication

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,7 +1,20 @@
+import { prisma } from '@/lib/prisma.js'
+import bcrypt from 'bcryptjs'
 import type { ILoginUser } from './auth.interface.js'
 
-const loginUserIntoDB = (payload: ILoginUser) => {
+const loginUserIntoDB = async (payload: ILoginUser) => {
 	const { email, password } = payload
+
+	const user = await prisma.user.findUniqueOrThrow({
+		where: { email }
+	})
+
+	if (!user) throw new Error('User not found')
+
+	const isPasswordMatched = await bcrypt.compare(password, user.password)
+	if (!isPasswordMatched) throw new Error('Invalid password')
+
+	return user
 }
 
 export const AuthServices = {
