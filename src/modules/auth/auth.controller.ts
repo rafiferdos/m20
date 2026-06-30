dd8fd8e4fbd3271e57a6b04b80a0d8d0a99679f5
Rafi Ferdos <rafiferdos@gmail.com>
2026-06-30 18:37:28 +0600

    feat: add cookie handling for access and refresh tokens in loginUser

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -8,6 +8,20 @@ const loginUser = catchAsync(async (req: Request, res: Response) => {
 	const payload = req.body
 	const result = await AuthServices.login(payload)
 
+	res.cookie('refreshToken', result.refreshToken, {
+		httpOnly: true,
+		secure: false, // Set to true in production when using HTTPS
+		sameSite: 'none',
+		maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
+	})
+
+	res.cookie('accessToken', result.accessToken, {
+		httpOnly: true,
+		secure: false, // Set to true in production when using HTTPS
+		sameSite: 'none',
+		maxAge: 15 * 60 * 1000 // 15 minutes
+	})
+
 	sendResponse(res, {
 		statusCode: status.OK,
 		message: 'User logged in successfully',
