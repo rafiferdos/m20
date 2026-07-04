ffa9be7fa526dd419bb97b69c68008c12b45ca18
Rafi Ferdos <rafiferdos@gmail.com>
2026-07-04 16:02:34 +0600

    feat: add ADMIN role to enums and update user route authorization

--- src/modules/user/user.route.ts
+++ src/modules/user/user.route.ts
@@ -1,9 +1,11 @@
+import { Role } from '@/generated/prisma/enums.js'
+import { auth } from '@/middlewares/auth.js'
 import { Router } from 'express'
 import { UserController } from './user.controller.js'
 
 const router = Router()
 
 router.post('/register', UserController.register)
-router.get('/me', UserController.getMyProfile)
+router.get('/me', auth(Role.AUTHOR, Role.USER), UserController.getMyProfile)
 
 export const userRoutes: Router = router
