58dd8353d64caff2f231aadb8905f07dd4cab809
Rafi Ferdos <rafiferdos@gmail.com>
2026-07-04 16:46:32 +0600

    refactor: format user route definitions for improved readability

--- src/modules/user/user.route.ts
+++ src/modules/user/user.route.ts
@@ -6,6 +6,15 @@ import { UserController } from './user.controller.js'
 const router = Router()
 
 router.post('/register', UserController.register)
-router.get('/me', auth(Role.AUTHOR, Role.USER, Role.ADMIN), UserController.getMyProfile)
+router.get(
+	'/me',
+	auth(Role.AUTHOR, Role.USER, Role.ADMIN),
+	UserController.getMyProfile
+)
+router.put(
+	'/my-profile',
+	auth(Role.AUTHOR, Role.USER, Role.ADMIN),
+	UserController.updateProfile
+)
 
 export const userRoutes: Router = router
