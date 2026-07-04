6015ba5d92f569e7a16f9065bc20839553ddc40a
Rafi Ferdos <rafiferdos@gmail.com>
2026-07-04 16:49:31 +0600

    refactor: improve formatting of IUser and IUserRegisterPayload interfaces for consistency

--- src/modules/user/user.interface.ts
+++ src/modules/user/user.interface.ts
@@ -1,18 +1,24 @@
-export interface IUser { 
-  id: string
-  name: string
-  email: string
-  password: string
-  activeStatus: TActiveStatus
-  role: TUserRole
+export interface IUser {
+	id: string
+	name: string
+	email: string
+	password: string
+	activeStatus: TActiveStatus
+	role: TUserRole
+	profile: {
+		profilePhoto: string | null
+		bio: string | null
+	}
+	createdAt: Date
+	updatedAt: Date
 }
 
 type TActiveStatus = 'ACTIVE' | 'BLOCKED'
 type TUserRole = 'USER' | 'AUTHOR'
 
 export interface IUserRegisterPayload {
-  name: string
-  email: string
-  password: string
-  profilePhoto?: string
+	name: string
+	email: string
+	password: string
+	profilePhoto?: string
 }
