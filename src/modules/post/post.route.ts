833c37c62ab5b6c421dd80ecb692defbfb76683a
Rafi Ferdos <rafiferdos@gmail.com>
2026-07-06 22:30:47 +0600

    refactor: enhance post route by adding authentication middleware

--- src/modules/post/post.route.ts
+++ src/modules/post/post.route.ts
@@ -1,8 +1,14 @@
+import { Role } from '@/generated/prisma/enums.js'
+import { auth } from '@/middlewares/auth.js'
 import { Router } from 'express'
 import { PostController } from './post.controller.js'
 
 const router = Router()
 
-router.post('/', PostController.create)
+router.post(
+	'/',
+	auth(Role.ADMIN, Role.AUTHOR, Role.USER),
+	PostController.create
+)
 
 export const PostRoutes: Router = router
