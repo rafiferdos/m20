45c7e6b46b73ddf2574c76bc215c7e6adbde4ba5
Rafi Ferdos <rafiferdos@gmail.com>
2026-07-06 22:06:49 +0600

    feat: enhance createPost function with response handling and status code

--- src/modules/post/post.controller.ts
+++ src/modules/post/post.controller.ts
@@ -1,10 +1,22 @@
-import catchAsync from "@/utils/catchAsync.js";
-import type { Request, Response } from "express";
+import catchAsync from '@/utils/catchAsync.js'
+import sendResponse from '@/utils/sendResponse.js'
+import type { Request, Response } from 'express'
+import status from 'http-status'
+import { PostService } from './post.service.js'
 
 const createPost = catchAsync(async (req: Request, res: Response) => {
-  
+	const id = req.user?.id
+	const payload = req.body
+
+	const result = await PostService.create(payload, id as string)
+
+	sendResponse(res, {
+		statusCode: status.CREATED,
+		message: 'Post created successfully',
+		data: result
+	})
 })
 
 export const PostController = {
-  create: createPost
+	create: createPost
 }
