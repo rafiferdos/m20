f837c8cb34079af04d4db73cc048c4eeb7531dad
Rafi Ferdos <rafiferdos@gmail.com>
2026-07-16 00:53:52 +0600

    refactor: remove error handling for user not found in getCommentsByAuthorIdFromDB

--- src/modules/comment/comment.service.ts
+++ src/modules/comment/comment.service.ts
@@ -1,6 +1,4 @@
 import { prisma } from '@/lib/prisma.js'
-import { AppError } from '@/utils/appError.js'
-import status from 'http-status'
 
 const getCommentsByAuthorIdFromDB = async (authorId: string) => {
 	const user = await prisma.user.findUniqueOrThrow({
@@ -11,7 +9,6 @@ const getCommentsByAuthorIdFromDB = async (authorId: string) => {
 			comments: true
 		}
 	})
-	if (!user) throw new AppError(status.NOT_FOUND, 'User not found')
 
 	const comments = user.comments
 	return comments
