39d1a8d502b1cf6bdab2ae6f75a78fa0cbcfe096
Rafi Ferdos <rafiferdos@gmail.com>
2026-07-16 00:49:53 +0600

    refactor: rename getCommentsByAuthorId to getAllByAuthorId in CommentController and update route accordingly

--- src/modules/comment/comment.controller.ts
+++ src/modules/comment/comment.controller.ts
@@ -4,19 +4,17 @@ import type { Request, Response } from 'express'
 import status from 'http-status'
 import { CommentService } from './comment.service.js'
 
-const getCommentsByAuthorId = catchAsync(
-	async (req: Request, res: Response) => {
-		const { authorId } = req.params
-		const comments = await CommentService.getAllByAuthorId(authorId as string)
+const getAllByAuthorId = catchAsync(async (req: Request, res: Response) => {
+	const { authorId } = req.params
+	const comments = await CommentService.getAllByAuthorId(authorId as string)
 
-		sendResponse(res, {
-			statusCode: status.OK,
-			message: 'Comments retrieved successfully',
-			data: comments
-		})
-	}
-)
+	sendResponse(res, {
+		statusCode: status.OK,
+		message: 'Comments retrieved successfully',
+		data: comments
+	})
+})
 
 export const CommentController = {
-	getAll: getCommentsByAuthorId
+	getAllByAuthorId
 }
--- src/modules/comment/comment.route.ts
+++ src/modules/comment/comment.route.ts
@@ -2,6 +2,6 @@ import { Router } from 'express'
 import { CommentController } from './comment.controller.js'
 const router = Router()
 
-router.get('/author/:authorId', CommentController.getAll)
+router.get('/author/:authorId', CommentController.getAllByAuthorId)
 
 export const CommentRoutes: Router = router
