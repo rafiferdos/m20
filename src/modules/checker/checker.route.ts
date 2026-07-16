464e3e2aea9e901aff1ec4475e2dc576b46212a5
Rafi Ferdos <rafiferdos@gmail.com>
2026-07-16 22:00:38 +0600

    feat: change check-role endpoint to use POST method with authentication

--- src/modules/checker/checker.route.ts
+++ src/modules/checker/checker.route.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express'
+import { auth } from '@/middlewares/auth.js'
 import { CheckerController } from './checker.controller.js'
 
 const router = Router()
 
-router.get('/check-role', CheckerController.checkRole)
+router.post('/check-role', auth(), CheckerController.checkRole)
 
 export const CheckerRoute: Router = router
