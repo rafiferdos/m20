5ae7ebc3000dc678d27fee80c57369f727c8f047
Rafi Ferdos <rafiferdos@gmail.com>
2026-07-16 22:00:35 +0600

    fix: update checkRoleFromDB to handle user not found error and adjust query

--- src/modules/checker/checker.service.ts
+++ src/modules/checker/checker.service.ts
@@ -4,17 +4,21 @@ import status from 'http-status'
 import type { IUserRole } from './checker.interface.js'
 
 const checkRoleFromDB = async (userId: string, role: IUserRole) => {
-	const user = await prisma.user.findUniqueOrThrow({
+	const user = await prisma.user.findUnique({
 		where: {
 			id: userId
 		},
 		select: {
+			id: true,
 			role: true
 		}
 	})
 
+	if (!user) throw new AppError(status.NOT_FOUND, 'User not found.')
 	if (user.role !== role.role)
 		throw new AppError(status.FORBIDDEN, 'Insufficient permissions')
+
+	return { userId: user.id, role: user.role, hasRole: user.role === role.role }
 }
 
 export const CheckerService = {
