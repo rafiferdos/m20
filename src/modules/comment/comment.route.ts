0d4178c99cc59501bb53944af7ccf8591b4305d9
Rafi Ferdos <rafiferdos@gmail.com>
2026-07-16 15:14:11 +0600

    feat: add POST route for creating comments in CommentRoutes

--- src/modules/comment/comment.route.ts
+++ src/modules/comment/comment.route.ts
@@ -3,5 +3,6 @@ import { CommentController } from './comment.controller.js'
 const router = Router()
 
 router.get('/author/:authorId', CommentController.getAllByAuthorId)
+router.post('/', CommentController.create)
 
 export const CommentRoutes: Router = router
