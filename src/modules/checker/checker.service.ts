76a49f76f2acc8acf7b5ce6a4b7c735a549e302d
Rafi Ferdos <rafiferdos@gmail.com>
2026-07-16 17:23:30 +0600

    style: format code for consistency in checkRoleFromDB function

--- src/modules/checker/checker.service.ts
+++ src/modules/checker/checker.service.ts
@@ -1,18 +1,21 @@
-import { prisma } from "@/lib/prisma.js"
-import { AppError } from "@/utils/appError.js"
-import status from "http-status"
+import { prisma } from '@/lib/prisma.js'
+import { AppError } from '@/utils/appError.js'
+import status from 'http-status'
 
 const checkRoleFromDB = async (userId: string, role: string) => {
-  const user = await prisma.user.findUniqueOrThrow({
-    where: {
-      id: userId
-    },
-    select: {
-      role: true
-    }
-  })
+	const user = await prisma.user.findUniqueOrThrow({
+		where: {
+			id: userId
+		},
+		select: {
+			role: true
+		}
+	})
 
-  if (user.role !== role) {
-    throw new AppError(status.FORBIDDEN, 'Insufficient permissions')
-  }
+	if (user.role !== role)
+		throw new AppError(status.FORBIDDEN, 'Insufficient permissions')
+}
+
+export const CheckerService = {
+	checkRole: checkRoleFromDB
 }
