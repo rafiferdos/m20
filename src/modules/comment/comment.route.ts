e1416b352e44ad3a175c1cc49272e057ffae2e1a
Rafi Ferdos <rafiferdos@gmail.com>
2026-07-16 16:04:40 +0600

    feat: add route to update comments by commentId in CommentRoutes

--- src/modules/comment/comment.route.ts
+++ src/modules/comment/comment.route.ts
@@ -7,5 +7,10 @@ const router = Router()
 router.get('/author/:authorId', CommentController.getAllByAuthorId)
 router.post('/', auth(Role.USER, Role.ADMIN), CommentController.create)
 router.get('/post/:postId', CommentController.getAllByPostId)
+router.patch(
+	'/:commentId',
+	auth(Role.USER, Role.ADMIN),
+	CommentController.update
+)
 
 export const CommentRoutes: Router = router
