e3e907186d40864012dc1fa1a427c6b68f432388
Rafi Ferdos <rafiferdos@gmail.com>
2026-07-21 17:26:24 +0600

    feat: add authentication middleware to checkout route

--- src/modules/subscription/subscription.route.ts
+++ src/modules/subscription/subscription.route.ts
@@ -1,8 +1,14 @@
+import { Role } from '@/generated/prisma/enums.js'
+import { auth } from '@/middlewares/auth.js'
 import { Router } from 'express'
 import { SubscriptionController } from './subscription.controller.js'
 
 const router = Router()
 
-router.post('/checkout', SubscriptionController.checkout)
+router.post(
+	'/checkout',
+	auth(Role.ADMIN, Role.USER, Role.AUTHOR),
+	SubscriptionController.checkout
+)
 
 export const SubscriptionRoute: Router = router
