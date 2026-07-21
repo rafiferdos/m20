575168d6265344e959b58c10fdd7f59f5a28e783
Rafi Ferdos <rafiferdos@gmail.com>
2026-07-21 18:17:37 +0600

    fix: add NextFunction type to createCheckoutSession for improved type safety

--- src/modules/subscription/subscription.controller.ts
+++ src/modules/subscription/subscription.controller.ts
@@ -1,11 +1,11 @@
 import catchAsync from '@/utils/catchAsync.js'
 import sendResponse from '@/utils/sendResponse.js'
-import type { Request, Response } from 'express'
+import type { NextFunction, Request, Response } from 'express'
 import status from 'http-status'
 import { SubscriptionService } from './subscription.service.js'
 
 const createCheckoutSession = catchAsync(
-	async (req: Request, res: Response) => {
+	async (req: Request, res: Response, next: NextFunction) => {
 		const userId = req.user?.id
 		const result = await SubscriptionService.create(userId as string)
 
