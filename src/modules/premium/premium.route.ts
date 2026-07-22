0df12c4dab37e4527708a121079a256afa41ac8e
Rafi Ferdos <rafiferdos@gmail.com>
2026-07-23 00:58:06 +0600

    feat: add premiumGuard middleware to PremiumRoutes for enhanced access control

--- src/modules/premium/premium.route.ts
+++ src/modules/premium/premium.route.ts
@@ -1,5 +1,6 @@
 import { Role } from '@/generated/prisma/enums.js'
 import { auth } from '@/middlewares/auth.js'
+import { premiumGuard } from '@/middlewares/premiumGuard.js'
 import { Router } from 'express'
 import { PremiumController } from './premium.controller.js'
 
@@ -8,6 +9,7 @@ const router = Router()
 router.get(
 	'/',
 	auth(Role.ADMIN, Role.USER, Role.AUTHOR),
+	premiumGuard(),
 	PremiumController.premium
 )
 
